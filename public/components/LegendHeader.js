ded1c3ac5454afc4fa3da726064f1e03f4a2f3f0
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-11-09 20:48:12 +0800

    for merging

---
+++ public/components/LegendHeader.js
@@ -0,0 +1,32 @@
+class LegendHeader extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const title = this.getAttribute("title");
+    const subtitle = this.getAttribute("subtitle") || "";
+
+    this.shadowRoot.innerHTML = `
+            <style>
+                h1 {
+                    font-size: 2.5rem;
+                    margin-bottom: 0.5rem;
+                }
+                p {
+                    font-size: 1.25rem;
+                    color: #666;
+                }
+            </style>
+            ${title ? `<h1>${title}</h1>` : ""}
+            <p>${subtitle}</p>
+        `;
+  }
+}
+
+customElements.define("csl-legend-header", LegendHeader);
